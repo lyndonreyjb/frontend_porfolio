26f5f1ae14ecac578d568ac8ca0020284534fe76
lyndonrey <lyndonreybualat@gmail.com>
2023-07-20 13:38:28 -0600

    update

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -63,7 +63,9 @@ const Navbar = () => {
                 const { id, url, icon } = socialIcon;
                 return (
                   <li key={id}>
-                    <a href={url}>{icon}</a>
+                    <a href={url} target="_blank" rel="noreferrer">
+                      {icon}
+                    </a>
                   </li>
                 );
               })}
