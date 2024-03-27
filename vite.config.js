d0be854f72bf26d713b1ee3b0366d7b843e25b06
lyndonrey <lyndonreybualat@gmail.com>
2024-03-27 16:33:30 -0600

    config updates

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import svgrPlugin from "vite-plugin-svgr";
 export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
-  base: "",
   root: "src",
   build: {
     outDir: "'../dist'",
